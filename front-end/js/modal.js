e12cdabd72b52a2c68d2f36b66e1956a28fde55a
santiange <santiangelica410@gmail.com>
2024-08-29 14:15:51 -0300

    modal

---
+++ front-end/js/modal.js
@@ -0,0 +1,17 @@
+const btnMail = document.getElementById("btn_mail")
+const modalContainer = document.getElementById("modal_container")
+const btnClose = document.getElementById("btn_close")
+
+btnMail.addEventListener("click", () => {
+    modalContainer.style.display = "flex"
+    document.body.style.position = "static"
+    document.body.style.height = "100%"
+    document.body.style.overflow = "hidden"
+})
+
+btnClose.addEventListener("click", () => {
+    modalContainer.style.display = "none"
+    document.body.style.position = "inherit"
+    document.body.style.height = "auto"
+    document.body.style.overflow = "visible"
+})
