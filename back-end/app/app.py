e75947c44cba83cf5898ed5a0cab8e83f596a3b0
santiange <santiangelica410@gmail.com>
2024-11-04 20:52:09 -0300

    clases para cotizacionesç

--- back-end/app/app.py
+++ back-end/app/app.py
@@ -12,12 +12,18 @@ def cotizaciones():
          response = requests.get("https://dolarapi.com/v1/cotizaciones")
          if response.status_code == 200:
              datos = response.json()
-             cotizacion = []
+             cotizacionArray = []
+
              for moneda in datos:
                    cotizacion = Cotizacion(moneda["compra"], moneda["venta"], moneda["fechaActualizacion"])
-                   tipomoneda = Tipo(moneda["moneda"], moneda["casa"],moneda["cotizacion"])
-                   cotizacion.append(tipomoneda)
-             return jsonify(cotizacion)
+                   tipomoneda = Tipo(moneda["nombre"] ,moneda["moneda"],moneda["casa"])
+                   cotizacion_data = {
+                    "cotizacion": cotizacion.to_dict(),
+                    "tipo": tipomoneda.to_dict()
+                    }
+                   cotizacionArray.append(cotizacion_data)
+
+             return jsonify(cotizacionArray)
          else:
             return jsonify({"error": "No se pudo obtener los datos"}), response.status_code
     except requests.exceptions.RequestException as e:
--- back-end/app/clases.py
+++ back-end/app/clases.py
@@ -1,9 +1,17 @@
 from abc import ABC, abstractmethod
 
 class Moneda(ABC):
-    def __init__(self, nombre):
+    def __init__(self, nombre, moneda):
         #self.nombre = nombre
         self.set_nombre(nombre)
+        self.set_moneda(moneda)
+    
+    def get_moneda(self):
+        return self.moneda
+    
+    def set_moneda(self, moneda):
+        self.moneda = moneda
+
 
     def get_nombre(self):
         return self.nombre
@@ -13,10 +21,9 @@ class Moneda(ABC):
 
 # Herencia
 class Tipo(Moneda):
-    def __init__(self, nombre, nombre_tipo, cotizaciones):
-        Moneda.__init__(self, nombre)
+    def __init__(self, nombre, moneda,  nombre_tipo):
+        Moneda.__init__(self, nombre, moneda)
         self.set_nombre_tipo(nombre_tipo)
-        self.set_cotizaciones(cotizaciones)
 
     def get_nombre_tipo(self):
         return self.nombre_tipo
@@ -33,6 +40,13 @@ class Tipo(Moneda):
 
     def add_cotizacion(self, cotizacion):
         self.cotizaciones.append(cotizacion)
+    
+    def to_dict(self):
+        return {
+            "moneda": self.moneda,
+            "casa": self.nombre_tipo,
+            "nombre": self.nombre
+        }
     
     def __str__(self):
         return f"Moneda: {self.get_nombre()} | Tipo de moneda: {self.get_nombre_tipo()}"
@@ -61,11 +75,17 @@ class Cotizacion:
 
     def set_fecha_actualizacion(self, fecha_actualizacion):
         self.fecha_actualizacion = fecha_actualizacion
+    
+    def to_dict(self):
+        return {
+            "compra": self.valor_compra,
+            "venta": self.valor_venta,
+            "fechaActualizacion": self.fecha_actualizacion
+        }
         
     def __str__(self):
         return f"Compra: {self.get_valor_compra()} | Venta: {self.get_valor_venta()} | Fecha Actualización: {self.get_fecha_actualizacion()}"
 
-moneda1 = Tipo('Dolar', 'Oficial', [])
-print(moneda1)
+
 
 
